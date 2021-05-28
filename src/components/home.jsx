cd58e2046486f968153daaf499d1b356882f4cce
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-05-28 14:05:20 -0700

    itemPage updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import ItemDetail from './itemDetail';
 
-export default function Home() {}
+export default function Home() {
+    const [allData, setAllData] = useState(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            // setAllData(data)
+            // console.log(allData)
+            return data;
+        })
+    });
+    
+
+    console.log(allData)
+    
+
+    // fetchData();
+
+
+    // useEffect(() => {
+    //     if (!allData) {
+    //         fetch('https://fakestoreapi.com/products')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             // console.log(data)
+    //             setAllData(data)
+    //             console.log(allData)
+    //         })
+    //     }
+        
+    // })
+    
+    return(
+        <ItemDetail />
+    )
+}
--- src/components/itemDetail.jsx
+++ src/components/itemDetail.jsx
@@ -2,8 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 function ItemDetail() {
     return (
-        <>
-            </>
+        <div className='detailPage'>
+            <img src='' alt=''></img>
+
+            <article>
+                <h3>Title</h3>
+                <p>Description</p>
+                <select placeholder='Size...'>
+                    <option value="XL"></option>
+                    <option value="L"></option>
+                    <option value="M"></option>
+                    <option value="S"></option>
+                    <option value="XS"></option>
+                </select>
+                <button>Add to cart</button>
+            </article>
+        </div>
     )
 }
 
