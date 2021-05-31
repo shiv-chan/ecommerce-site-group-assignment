ac4641807420a7286f27618d0027686557b59df7
Dannyboy222 <76968917+Dannyboy222@users.noreply.github.com>
2021-05-31 15:07:46 -0700

    updated package.json

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function ItemCard(props) {
 	return (
+		<article key={props.key}>
 			<img src={props.imgURL} alt={props.title} style={props.imgSize} />
 			<h2>{props.title}</h2>
 			<h3>{props.price}</h3>
